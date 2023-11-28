f2e96aaf9cb27219022bbb82f972e0f9abf0d9fa
Rohan Saini <rohan@Rohans-MacBook-Pro.local>
2023-11-28 15:08:24 -0500

    port changed

--- server.js
+++ server.js
@@ -72,7 +72,7 @@ app.post('/login', async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
